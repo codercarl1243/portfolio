fabbd6f838f7133063e919b80f5c15e90fb55c91
codercarl1243 <codercarl1243@gmail.com>
2024-12-18 19:55:04 +1000

    add custom Image component

--- components/image.tsx
+++ components/image.tsx
@@ -2,32 +2,35 @@
 import { urlFor } from '@/sanity/lib/image';
 import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 import Image from 'next/image';
+import { ComponentProps } from 'react';
+import cx from 'classnames';
 
 type TProps = {
     width?: number;
     height?: number;
     alt?: string;
-    imageAsset: SanityImageSource;
-}
+    sanityImage: SanityImageSource;
+} & Omit<ComponentProps<typeof Image>, 'src' >
 
 export default async function Main({
-    width = 800, height = 300, alt = "", imageAsset
+    width = 800, height = 300, alt = "", sanityImage, className
 }: TProps
 ) {
-    if (!imageAsset) {
+    
+    if (!sanityImage) {
         return;
     }
 
     return (
         <Image
-            src={urlFor(imageAsset)
+            src={urlFor(sanityImage)
                 .width(width)
                 .height(height)
                 .url()}
             alt={alt}
             width={width}
-            height={height}
-            className='image'
+            height={height} 
+            className={cx('image block-image', className)}
         />
     )
 }
