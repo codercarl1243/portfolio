d74e65d9ca05933c3ee7033258ef10feda268d14
codercarl1243 <codercarl1243@gmail.com>
2024-12-18 19:59:12 +1000

    ADD common  client side query method

--- sanity/lib/groq.ts
+++ sanity/lib/groq.ts
@@ -1,40 +1,31 @@
-import { client } from './client';
+import { POST_QUERYResult, POSTS_QUERYResult } from "../types/sanity.types";
 import { POST_QUERY, POSTS_QUERY } from "./sanity.queries";
 import { sanityFetch } from "@/app/studio/live";
 
-export async function getAllPosts() {
+export const getAllPosts = async () => await _SanityFetch<POSTS_QUERYResult[]>(POSTS_QUERY);
 
+export const getPost = async (slug: string) => await _SanityFetch<POST_QUERYResult>(POST_QUERY, { slug })
+
+
+async function _SanityFetch<T>(query: string, params?: Record<string, string>): Promise<T>{
     try {
-        const result = await client.fetch(POSTS_QUERY);
-        if (!result) return [];
-
-        return result;
-
-    } catch (err) {
-        console.log("error occured in fetching Posts \n", err)
-    }
-}
-
-export async function getPost(slug: string) {
-
-    try {
-        // console.log("GET POST slug", slug)
         const {data} = await sanityFetch({
-            query: POST_QUERY,
-            params: { slug: 'post1' }
+            query: query,
+            params: { ...params}
         })
 
-        // const result = await client.fetch(POST_QUERY, { slug });
-        // console.log("RESULT ", result)
-        // console.log("GET POST", data)
         if (!data) {
-            console.warn(`No post found for slug: ${slug}`);
-            return null;
+            throw new Error()
         }
 
-        return data;
+    return data
 
     } catch (err) {
-        console.log(`error occured in fetching Post: ${slug} \n`, err)
+        console.error("Error occurred in Sanity fetch:", {
+            query,
+            params,
+            error: err,
+        });
+        throw err; // Re-throw the error so the caller can handle it
     }
 }
