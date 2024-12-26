4a5b49b6ca4d74760938b7bfe5e556088620f605
codercarl1243 <codercarl1243@gmail.com>
2024-12-26 16:35:52 +1000

    add basic code component, styling being provided by tailwind

--- components/code.tsx
+++ components/code.tsx
@@ -1,6 +1,8 @@
 import { CodeInputValue } from "@sanity/code-input";
 
-export default function CodeBlock({ language, code }: CodeInputValue) {
+export type TCodeProps = CodeInputValue;
+
+export default function CodeBlock({ language, code }: TCodeProps) {
 
     return (
             <pre className='bg-gray-50 border p-2'>
