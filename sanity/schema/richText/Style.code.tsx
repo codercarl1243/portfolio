28084ff9b3cde979eb66f0357d8c45e7f741b5f1
codercarl1243 <codercarl1243@gmail.com>
2025-01-03 15:23:39 +1000

    separate all richtext components into relevant files

---
+++ sanity/schema/richText/Style.code.tsx
@@ -0,0 +1,12 @@
+import { GrCode } from "react-icons/gr"
+
+export const code = {
+    name: "code",
+    title: "Code Block",
+    type: 'code', 
+    icon: GrCode, 
+    options: {
+        withFilename: true,
+        highlightedLines: true,
+    }
+}
---
+++ sanity/schema/richText/Style.image.tsx
@@ -0,0 +1,1 @@
+export const image = { type: 'CustomImage'}
